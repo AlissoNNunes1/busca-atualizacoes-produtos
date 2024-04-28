e29671c3f76dbdcd4c7810d825ae201ba1e007ba
AlissoNNunes1 <alissonandradennnunes@gmail.com>
2024-04-28 12:55:00 -0300

    Update listarProdutos to include dataAtual parameter in request Update detalhesProdutos to accept gtin parameter instead of dataAtual and gtin Remove buscarProdutos function

--- app.js
+++ app.js
@@ -1,30 +1,38 @@
 // Importar o módulo de renderização de templates EJS
 const ejs = require('ejs');
 const express = require('express');
-const { listarProdutos, detalhesProdutos } = require('./service');
+const { listarProdutos, detalhesProduto } = require('./service');
 const app = express();
+const now = new Date();
+const year = now.getFullYear();
+const month = (now.getMonth() + 1).toString().padStart(2, '0');
+const day = now.getDate().toString().padStart(2, '0');
+
+const dataAtual = `${year}${month}${day}${"00"}${"00"}${"00"}`;
 
 // Rota que retorna os produtos atualizados no dia com informações
 app.get('/', async (req, res) => {
     try {
-        // Lista os produtos atualizados no dia
-        const gtinsAtualizados = await listarProdutos();
+       // Lista os produtos atualizados no dia
+       const gtinsAtualizados = await listarProdutos(dataAtual);
+        
+       // Array que armazena o detalhe dos produtos
+       const detalhesProdutos = [];
 
-        // Array que armazena o detalhe dos produtos
-        const detalhesProdutos = [];
-
-        //Intera sobre cada GTIN e realiza a busca sobre os detalhes de cada um
-        for (const gtin of gtinsAtualizados) {
-            const detalhes = await detalhesProdutos(gtin);
-            
-            detalhesProdutos.push(detalhes);
-        }
-        console.log(detalhesProdutos)
-        // Renderizar o template 'index' e passar a variável 'produtos' para o template
-        res.json(detalhesProdutos);
-    } catch (error) {
-        res.status(500).json({ error: 'Erro ao buscar produtos atualizados' });
-    }
+       //Intera sobre cada GTIN e realiza a busca sobre os detalhes de cada um
+       for (const gtin of gtinsAtualizados) {
+           const code = gtin.gtin
+           console.log(code)
+           const detalhes = await detalhesProduto(code);
+           
+           detalhesProdutos.push(detalhes);
+       }
+       
+       // Renderizar o template 'index' e passar a variável 'produtos' para o template
+       res.json(detalhesProdutos);
+   } catch (error) {
+       res.status(500).json({ error: 'Erro ao buscar produtos atualizados' });
+   }
 });
 
 const PORT = process.env.PORT || 3000;
