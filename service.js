49e6298bfb05164d42d4b0cfc3230958fa744754
Leonardo-SF7 <Leonardo213243@hotmail.com>
2024-05-06 19:07:23 -0300

    detalhes

--- service.js
+++ service.js
@@ -56,77 +56,7 @@ async function listarProdutos(dataAtual, token) {
            
             // Retorna uma lista de objetos contendo GTIN e descrição de cada produto
             return response.data.map(produto => ({
-
                 gtin: produto.gtin,
-                descricao: produto.descricao,
-                marca: produto.marca ? produto.marca.submarca : null,
-                submarca: produto.marca ? produto.marca.submarca : null,
-                codigoInterno: produto.codigoInterno,
-                categoriaProduto: produto.categoriaProduto ? produto.categoriaProduto.nome : null,
-                categoriaPai: produto.categoriaPai ? produto.categoriaPai.nome : null,
-                origem: produto.origem,
-
-                classificacaoFiscal: produto.classificacaoFiscal ? {
-                    ncm: produto.classificacaoFiscal.ncm,
-                    caracteristica: produto.classificacaoFiscal.caracteristica,
-                    cestCodigo: produto.classificacaoFiscal.cestCoding,
-                    cestDescricao: produto.classificacaoFiscal.cestDescricao,
-                } : null,
-
-                imagemPrincipal: produto.imagemPrincipal ? {
-                    url: produto.imagemPrincipal.url,
-                    ultimaAtualizacao: produto.imagemPrincipal.ultimaAtualizacao,
-                } : null,
-
-                ativos: produto.ativos ? {
-                    url: produto.ativos.url,
-                    ultimaAtualizacao: produto.ativos.ultimaAtualizacao,
-                } : null,
-
-                composicaoLogistica: produto.composicaologistica ? {
-                    gtin: produto.composicaologistica.gtin,
-                    unidadeEmbalagem: produto.composicaologistica.unidadeEmbalagen,
-                    quantidade: produto.composicaologistica.quantidade,
-                    quantidadeTotal: produto.composicaoLogistica.quantidadeTotal,
-                    altura: produto.composicaologistica.altura,
-                    alturaUm: produto.composicaologistica.alturaum ? {
-                        abrev: produto.composicaologistica.alturaum.abrev,
-                        nome: produto.composicaologistica.alturalm.nome,
-                        baseConversao: produto.composicaoLogistica.baseConvercao,
-                    largura: produto.largura,
-                    largauraUm: produto.larguraUm ? {
-                        nome: produto.larguraUm.nome,
-                        abrev: produto.larguraUm.abrev,
-                        baseConversao: produto.larguraUm.baseConvercao,
-                        } : null,
-                    } : null,
-
-                    profundidade: produto.profundidade,
-                    profundidadeUm: produto.profundidadeUm ? {
-                        nome: produto.profundidadeUm.nome,
-                        abrev: produto.profundidadeUmUm.abrev,
-                        baseConversao: produto.profundidadeUm.baseConvercao,
-                    } : null,
-
-                    pesoLiquido: produto.pesoLiquido,
-                    pesoLiquidoUm: produto.pesoLiquidoUm ? {
-                        nome: produto.pesoLiquidoUm.nome,
-                        abrev: produto.pesoLiquidoUm.abrev,
-                        baseConversao: produto.pesoLiquidoUm.baseConvercao,
-                    } : null,
-
-                    pseoBruto: produto.pseoBruto,
-                    pesoBrutoUm: produto.pesoBrutoUm ? {
-                        nome: produto.pesoBrutoUm.nome,
-                        abrev: produto.pesoBrutoUm.abrev,
-                        baseConversao: produto.pesoBrutoUm.baseConvercao,
-                    } : null,
-
-                    quantidadeCamadasPallet: produto.quantidadeCamadasPallet !== undefined ? produto.quantidadeCamadasPallet : null,
-                    caixasCamada: produto.caixasCamada !== undefined ? produto.caixasCamada : null,
-                    alturaPallet: produto.alturaPallet !== undefined ? produto.alturaPallet : null,
-                    larguraPallet: produto.larguraPallet !== undefined ? produto.larguraPallet : null,
-                } : null,
                 
             }));
             
@@ -154,8 +84,80 @@ async function detalhesProduto(gtin, token) {
         //console.log(response.data);
 
         // Extrair os detalhes relevantes do produto da resposta
-        const detalhes = response.data;
+        const detalhes = response.data.map(produto => ({
 
+            gtin: produto.gtin,
+            descricao: produto.descricao,
+            marca: produto.marca ? produto.marca.submarca : null,
+            submarca: produto.marca ? produto.marca.submarca : null,
+            codigoInterno: produto.codigoInterno,
+            categoriaProduto: produto.categoriaProduto ? produto.categoriaProduto.nome : null,
+            categoriaPai: produto.categoriaPai ? produto.categoriaPai.nome : null,
+            origem: produto.origem,
+
+            classificacaoFiscal: produto.classificacaoFiscal ? {
+                ncm: produto.classificacaoFiscal.ncm,
+                caracteristica: produto.classificacaoFiscal.caracteristica,
+                cestCodigo: produto.classificacaoFiscal.cestCoding,
+                cestDescricao: produto.classificacaoFiscal.cestDescricao,
+            } : null,
+
+            imagemPrincipal: produto.imagemPrincipal ? {
+                url: produto.imagemPrincipal.url,
+                ultimaAtualizacao: produto.imagemPrincipal.ultimaAtualizacao,
+            } : null,
+
+            ativos: produto.ativos ? {
+                url: produto.ativos.url,
+                ultimaAtualizacao: produto.ativos.ultimaAtualizacao,
+            } : null,
+
+            composicaoLogistica: produto.composicaologistica ? {
+                gtin: produto.composicaologistica.gtin,
+                unidadeEmbalagem: produto.composicaologistica.unidadeEmbalagen,
+                quantidade: produto.composicaologistica.quantidade,
+                quantidadeTotal: produto.composicaoLogistica.quantidadeTotal,
+                altura: produto.composicaologistica.altura,
+                alturaUm: produto.composicaologistica.alturaum ? {
+                    abrev: produto.composicaologistica.alturaum.abrev,
+                    nome: produto.composicaologistica.alturalm.nome,
+                    baseConversao: produto.composicaoLogistica.baseConvercao,
+                largura: produto.largura,
+                largauraUm: produto.larguraUm ? {
+                    nome: produto.larguraUm.nome,
+                    abrev: produto.larguraUm.abrev,
+                    baseConversao: produto.larguraUm.baseConvercao,
+                    } : null,
+                } : null,
+
+                profundidade: produto.profundidade,
+                profundidadeUm: produto.profundidadeUm ? {
+                    nome: produto.profundidadeUm.nome,
+                    abrev: produto.profundidadeUmUm.abrev,
+                    baseConversao: produto.profundidadeUm.baseConvercao,
+                } : null,
+
+                pesoLiquido: produto.pesoLiquido,
+                pesoLiquidoUm: produto.pesoLiquidoUm ? {
+                    nome: produto.pesoLiquidoUm.nome,
+                    abrev: produto.pesoLiquidoUm.abrev,
+                    baseConversao: produto.pesoLiquidoUm.baseConvercao,
+                } : null,
+
+                pseoBruto: produto.pseoBruto,
+                pesoBrutoUm: produto.pesoBrutoUm ? {
+                    nome: produto.pesoBrutoUm.nome,
+                    abrev: produto.pesoBrutoUm.abrev,
+                    baseConversao: produto.pesoBrutoUm.baseConvercao,
+                } : null,
+
+                quantidadeCamadasPallet: produto.quantidadeCamadasPallet !== undefined ? produto.quantidadeCamadasPallet : null,
+                caixasCamada: produto.caixasCamada !== undefined ? produto.caixasCamada : null,
+                alturaPallet: produto.alturaPallet !== undefined ? produto.alturaPallet : null,
+                larguraPallet: produto.larguraPallet !== undefined ? produto.larguraPallet : null,
+            } : null,
+            
+        }));
         
         
         return detalhes;
