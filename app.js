3cce87c896f0a33f464b77817774668b64c31a97
AlissoNNunes1 <alissonandradennnunes@gmail.com>
2024-05-19 23:14:04 -0300

    chore: Add caching for product details to improve performance

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-// Importar o módulo de renderização de templates EJS
-const ejs = require("ejs");
+
 const express = require("express");
 const swaggerUi = require("swagger-ui-express");
 const specs = require("./swaggerConfig");
 const cron = require('node-cron');
+const NodeCache = require('node-cache');
 const { listarProdutos, detalhesProduto, autenticar } = require("./service");
 const app = express();
+
+const cache = new NodeCache({ stdTTL: 3600 }); // Cache com TTL de 1 hora
 
 const obterDataAtual = () => {
   const now = new Date();
@@ -14,6 +16,17 @@ const obterDataAtual = () => {
   const day = now.getDate().toString().padStart(2, '0');
   return `${year}${month}${day}${"00"}${"00"}${"00"}`;
 };
+
+// Middleware para medir o tempo de resposta
+app.use((req, res, next) => {
+  const start = process.hrtime();
+  res.on('finish', () => {
+    const elapsed = process.hrtime(start);
+    const elapsedMs = elapsed[0] * 1000 + elapsed[1] / 1e6;
+    console.log(`Tempo de resposta para [${req.method}] ${req.originalUrl}: ${elapsedMs.toFixed(3)} ms`);
+  });
+  next();
+});
 
 // Definindo a tarefa agendada para ser executada à meia-noite todos os dias
 cron.schedule('0 0 * * *', async () => {
@@ -39,13 +52,25 @@ app.get("/", async (req, res) => {
     const token = await autenticar();
     const dataAtual = obterDataAtual();
 
-    // Lista os produtos atualizados no dia
-    const gtinsAtualizados = await listarProdutos(dataAtual, token);
+    // Verifica se os dados estão no cache
+    const cacheKey = `produtos-${dataAtual}`;
+    let detalhesProdutos = cache.get(cacheKey);
 
-    // Array que armazena o detalhe dos produtos
-    const detalhesProdutos = await Promise.all(
-      gtinsAtualizados.map(gtin => detalhesProduto(gtin.gtin, token))
-    );
+    if (!detalhesProdutos) {
+      // Lista os produtos atualizados no dia
+      const gtinsAtualizados = await listarProdutos(dataAtual, token);
+
+      // Array que armazena o detalhe dos produtos
+      detalhesProdutos = await Promise.all(
+        gtinsAtualizados.map(gtin => detalhesProduto(gtin.gtin, token))
+      );
+
+      // Armazena os detalhes no cache
+      cache.set(cacheKey, detalhesProdutos);
+      console.log("Dados armazenados no cache");
+    } else {
+      console.log("Dados recuperados do cache");
+    }
 
     res.json(detalhesProdutos);
   } catch (error) {
